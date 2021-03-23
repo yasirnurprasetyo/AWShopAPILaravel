1cd2093c2a9827023ca34656ecbe856a71a3f5c5
yasirnurprasetyo <yasirnurprasetyo@gmail.com>
2021-03-23 21:53:14 +0700

    Login, Regster with validation

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -25,6 +26,35 @@ class UserController extends Controller
             return $this->error("Password Salah");
         }
         return $this->error("Email tidak terdaftar");
+    }
+
+    public function register(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required|min:8'
+        ]);
+
+        if($validation->fails()){
+            $val = $validation->errors()->all();
+            return $this->error($val[0]);
+        }
+
+        $user = User::create(array_merge($request->all(), [
+            'password' => bcrypt($request->password)
+        ]));
+
+
+        if($user){
+            return response()->json([
+                'success' => 1,
+                'message' => 'Selamat datang register berhasil ',
+                'user' => $user
+            ]);
+        }
+
+        return $this->error('Registrasi Gagal');
     }
 
     public function error($message)
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('login', 'Api\UserController@login');
+Route::post('register', 'Api\UserController@register');
